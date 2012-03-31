2443bdb652d1e9d5ec7571a5c663a175cb675d2f
John Ivanoff <john.ivanoff@gmail.com>
2012-03-30 20:41:59 -0500

    seting up calculation and adding it to db

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Milage
+  include DataMapper::Resource
+  property :id,          Serial
+  property :mpg,         Numeric, :required => true
+  property :miles,       Numeric, :required => true
+  property :gallons,     Numeric, :required => true
+  property :created_at,  DateTime
+  property :updated_at,  DateTime
+end
+
+DataMapper.finalize
+
+get '/' do
+  "Welcome to my page!"
+end
+
+post '/' do 
+  mpg = params[:miles].to_f / params[:gallons].to_f
+  fill_up = Milage.new(:miles => params[:miles], :gallons => params[:gallons], :mpg => mpg)
+  if fill_up.save
+    status 201
+  else
+    status 412
+  end
+end
